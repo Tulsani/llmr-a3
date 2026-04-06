4c2accbe0e0b52005723ccedc781cc0b25d26596
Akshat Tulsani <tulsani@mac.mynetworksettings.com>
2026-04-06 15:46:34 -0400

    updated adapters for test

--- tests/adapters.py
+++ tests/adapters.py
@@ -7,7 +7,7 @@ import torch
 from torch import Tensor
 from torch.utils.data import Dataset
 from transformers import PreTrainedTokenizerBase
-from student.sft_helpers import tokenize_prompt_and_output
+from student.sft_helpers import tokenize_prompt_and_output,compute_entropy
 
 def run_tokenize_prompt_and_output(
     prompt_strs: list[str],
@@ -82,7 +82,7 @@ def run_compute_group_normalized_rewards(
 
 def run_compute_entropy(logits: torch.Tensor) -> torch.Tensor:
     """Get the entropy of the logits (i.e., entropy of the final dimension)."""
-    raise NotImplementedError
+    return compute_entropy(logits=logits)
 
 
 def run_get_response_log_probs(
