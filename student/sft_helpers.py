88ed9c48610ae975773bd3ad869d385b91b1cf3c
Akshat Tulsani <tulsani@mac.mynetworksettings.com>
2026-04-06 05:52:31 -0400

    part1 test

---
+++ student/sft_helpers.py
@@ -0,0 +1,43 @@
+import torch
+
+
+def tokenize_prompt_and_output(prompt_strs, output_strs, tokenizer) -> dict[str, torch.Tensor]:
+    #tokenizing input and response
+    tokenized_prompt = tokenizer(prompt_strs, add_special_tokens=True)["input_ids"]
+    tokenized_output = tokenizer(output_strs, add_special_tokens=False)["input_ids"]
+    
+    #input_ids and response_list
+    input_ids_list = []
+    response_mask_list = []
+
+    for prompt_ids,output_ids in zip(tokenized_prompt,tokenized_output):
+        combined = prompt_ids + output_ids # concating indivusal prompt and inputs
+        input_ids_list.append(combined)
+
+        #mask 0 for prompt token and 1 for response tokens
+        mask = [0] * len(prompt_ids) + [1] * len(output_ids)
+        response_mask_list.append(mask)
+    
+    # padding to max lenght in each batch
+    max_len = max(len(ids) for ids in input_ids_list)
+    # setting default pad id
+    pad_id = tokenizer.pad_token_id if tokenizer.pad_token_id is not None else 0
+
+    padded_inputs_ids = []
+    padded_masks = []
+
+    for ids,mask in zip(input_ids_list,response_mask_list):
+        pad_len = max_len - len(ids)
+        padded_inputs_ids.append(ids + [pad_id]*pad_len)
+        padded_masks.append(mask + [0]*pad_len)
+
+    # convert to tensors
+    input_ids_tensor = torch.tensor(padded_inputs_ids) # (batch_size,max_len)
+    mask_tensor = torch.tensor(padded_masks)
+
+    #response mask
+    return{
+        "input_ids": input_ids_tensor[:,:-1],
+        "labels": input_ids_tensor[:,1:],
+        "response_mask":mask_tensor[:,1:]
+    }
