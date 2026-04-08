39fcc96feff705081f1badbb0764fec0a3bfb307
Akshat Tulsani <tulsani@mac.mynetworksettings.com>
2026-04-07 22:01:57 -0400

    grpo adavantages fn

--- student/grpo_helpers.py
+++ student/grpo_helpers.py
@@ -6,8 +6,42 @@ def compute_group_normalized_rewards(reward_fn,
                                      group_size,
                                      advantage_eps,
                                      normalize_by_std):
+    raw_rewards= []
+    ## computing raw rewards
+    for respone,ground_truth in zip(rollout_responses,repeated_ground_truths):
+        reward_dict = reward_fn(respone, ground_truth)
+        raw_rewards.append(reward_dict["reward"])
     
-    return
+    assert len(raw_rewards) % group_size == 0, "raw rewards not divisible by group size"
+
+    rollout_batch_size = len(raw_rewards)
+    n_groups = rollout_batch_size // group_size
+    ## raw reward to tensor
+    raw_rewards_tensor = torch.tensor(raw_rewards, dtype=torch.float32)
+    grouped = raw_rewards_tensor.reshape(n_groups, group_size)
+
+    group_mean = grouped.mean(dim=-1)
+
+    ## pertoken adv
+    if normalize_by_std:
+        
+        std_across_group = torch.std(grouped,dim=-1)
+        
+        advantage_across_group = (grouped - group_mean.unsqueeze(-1)) / (std_across_group.unsqueeze(-1)+advantage_eps)
+    else:
+
+        advantage_across_group = raw_rewards - group_mean.unsqueeze(-1)
+    
+    advantages = advantage_across_group.reshape(rollout_batch_size)
+
+    metadata = {
+        "mean_reward": raw_rewards_tensor.mean().item(),
+        "std_reward":  raw_rewards_tensor.std().item(),
+        "max_reward":  raw_rewards_tensor.max().item(),
+        "min_reward":  raw_rewards_tensor.min().item(),
+    }
+
+    return advantages, raw_rewards_tensor, metadata
 
 def compute_naive_policy_gradient_loss(raw_rewards_or_advantages,
                                        policy_log_probs):
--- tests/adapters.py
+++ tests/adapters.py
@@ -8,6 +8,7 @@ from torch import Tensor
 from torch.utils.data import Dataset
 from transformers import PreTrainedTokenizerBase
 from student.sft_helpers import tokenize_prompt_and_output,compute_entropy,get_response_log_probs,masked_normalize,sft_microbatch_train_step
+from student.grpo_helpers import compute_group_normalized_rewards
 
 def run_tokenize_prompt_and_output(
     prompt_strs: list[str],
@@ -77,7 +78,12 @@ def run_compute_group_normalized_rewards(
                 You may choose what you wish to log here
                 (some statistics of the rewards, etc.).
     """
-    raise NotImplementedError
+    return compute_group_normalized_rewards(reward_fn=reward_fn,
+                                            rollout_responses=rollout_responses,
+                                            repeated_ground_truths=repeated_ground_truths,
+                                            group_size=group_size,
+                                            advantage_eps=advantage_eps,
+                                            normalize_by_std=normalize_by_std)
 
 
 def run_compute_entropy(logits: torch.Tensor) -> torch.Tensor:
